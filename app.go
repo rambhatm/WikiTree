4a6b24e5a28a9c9a0ebf1d46683e1a6720537d88
rambhatm <rammulky@gmail.com>
2020-05-27 02:17:08 +0530

    working, key + value

--- app.go
+++ app.go
@@ -12,15 +12,15 @@ import (
 
 var url string = "https://en.wikipedia.org/wiki/Coronavirus_disease_2019"
 var allowedDomain string = "en.wikipedia.org"
+var numLinksFound int = 0
 
-func crawlerSummary(start time.Time, numLinksFound int) {
+func crawlerSummary(start time.Time) {
 	elapsed := time.Since(start)
 	log.Printf("\nCrawler Summary\nTop-level URL\t%s\nTime taken\t%s\nFound\t%d\n", url, elapsed, numLinksFound)
 }
 
 func main() {
-	var numLinksFound int = 0
-	defer crawlerSummary(time.Now(), numLinksFound)
+	defer crawlerSummary(time.Now())
 	db, err := bolt.Open("my.db", 0600, nil)
 	if err != nil {
 		log.Fatal(err)
@@ -44,18 +44,19 @@ func main() {
 			regexp.MustCompile("https://en.wikipedia\\.org/wiki/Special\\:"),
 			regexp.MustCompile("https://en.wikipedia\\.org/wiki/Category\\:"),
 			regexp.MustCompile("https://en.wikipedia\\.org/wiki/Template_talk\\:"),
+			regexp.MustCompile("https://en.wikipedia\\.org/wiki/Portal\\:"),
+			regexp.MustCompile("https://en.wikipedia\\.org/wiki/Talk\\:"),
 		),
 	)
 
 	// Find and visit all links
 	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
 		e.Request.Visit(e.Attr("href"))
-
 	})
 
-	c.OnRequest(func(r *colly.Request) {
+	c.OnHTML("title", func(e *colly.HTMLElement) {
 		numLinksFound = numLinksFound + 1 //TODO hmmm, is this goroutine safe?
-		fmt.Println("Visiting", r.URL)
+		fmt.Printf("URL: %s Title: %s\n", e.Request.URL, e.Text)
 	})
 
 	c.Visit(url)
