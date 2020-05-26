c23e2d2ce5335e31f74fc0d837629f263e83e1e4
Ram Bhat <ram@MacBook-Air.local>
2020-05-27 01:18:08 +0530

    async + summary

--- app.go
+++ app.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"regexp"
+	"time"
 
 	"github.com/boltdb/bolt"
 	"github.com/gocolly/colly"
@@ -12,7 +13,14 @@ import (
 var url string = "https://en.wikipedia.org/wiki/Coronavirus_disease_2019"
 var allowedDomain string = "en.wikipedia.org"
 
+func crawlerSummary(start time.Time, numLinksFound int) {
+	elapsed := time.Since(start)
+	log.Printf("\nCrawler Summary\nTop-level URL\t%s\nTime taken\t%s\nFound\t%d\n", url, elapsed, numLinksFound)
+}
+
 func main() {
+	var numLinksFound int = 0
+	defer crawlerSummary(time.Now(), numLinksFound)
 	db, err := bolt.Open("my.db", 0600, nil)
 	if err != nil {
 		log.Fatal(err)
@@ -21,9 +29,21 @@ func main() {
 
 	c := colly.NewCollector(
 		colly.AllowedDomains(allowedDomain),
+		colly.Async(true),
 		colly.MaxDepth(2),
 		colly.URLFilters(
 			regexp.MustCompile("https://en.wikipedia\\.org/wiki/"),
+		),
+
+		colly.DisallowedURLFilters(
+			regexp.MustCompile("https://en.wikipedia\\.org/wiki/File\\:"),
+			regexp.MustCompile("https://en.wikipedia\\.org/wiki/Template\\:"),
+			regexp.MustCompile("https://en.wikipedia\\.org/wiki/Help\\:"),
+			regexp.MustCompile("https://en.wikipedia\\.org/wiki/VideoWiki\\:"),
+			regexp.MustCompile("https://en.wikipedia\\.org/wiki/Wikipedia\\:"),
+			regexp.MustCompile("https://en.wikipedia\\.org/wiki/Special\\:"),
+			regexp.MustCompile("https://en.wikipedia\\.org/wiki/Category\\:"),
+			regexp.MustCompile("https://en.wikipedia\\.org/wiki/Template_talk\\:"),
 		),
 	)
 
@@ -34,8 +54,10 @@ func main() {
 	})
 
 	c.OnRequest(func(r *colly.Request) {
+		numLinksFound = numLinksFound + 1 //TODO hmmm, is this goroutine safe?
 		fmt.Println("Visiting", r.URL)
 	})
 
 	c.Visit(url)
+	c.Wait()
 }
