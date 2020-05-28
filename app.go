fb4c4cc967389bfd7dbedf130737c5f38b5686a1
rambhatm <rammulky@gmail.com>
2020-05-28 15:39:07 +0530

    remove encoder

--- app.go
+++ app.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"bytes"
 	"context"
-	"encoding/gob"
 	"log"
 
 	"go.mongodb.org/mongo-driver/mongo"
@@ -24,20 +22,10 @@ type WikiDoc struct {
 var url string = "https://en.wikipedia.org/wiki/Coronavirus_disease_2019"
 
 func (doc WikiDoc) InsertDB(client *mongo.Client) {
-	var value bytes.Buffer
-	encoder := gob.NewEncoder(&value)
-
-	//encode
-	err := encoder.Encode(WikiDoc{doc.Title})
-	if err != nil {
-		log.Fatal("encode error:", err)
-		return
-	}
-
 	crawlColly := client.Database(WIKIDB).Collection(CRAWLRESULTS)
 
 	//Find a max of 5 results for filter
-	_, err = crawlColly.InsertOne(context.TODO(), doc)
+	_, err := crawlColly.InsertOne(context.TODO(), doc)
 	if err != nil {
 		log.Fatal(err)
 	}
--- go.mod
+++ go.mod
@@ -13,6 +13,7 @@ require (
 	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
 	github.com/tecbot/gorocksdb v0.0.0-20191217155057-f0fad39f321c // indirect
 	github.com/temoto/robotstxt v1.1.1 // indirect
+	go.mongodb.org/mongo-driver v1.3.3 // indirect
 	golang.org/x/net v0.0.0-20200520182314-0ba52f642ac2 // indirect
 	golang.org/x/sys v0.0.0-20200523222454-059865788121 // indirect
 	google.golang.org/appengine v1.6.6 // indirect
